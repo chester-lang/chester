ea6ba9b56bf162ba5517cf3c44535313ffa77e1d
Mio <mio@mio19.uk>
2025-12-04 16:13:43 +1100

    Nat

---
+++ modules/utils/shared/src/main/scala/chester/utils/Nat.scala
@@ -0,0 +1,21 @@
+package chester.utils
+
+import spire.math.Natural
+
+import scala.util.FromDigits
+import scala.util.FromDigits.NumberTooSmall
+
+object Nat {
+  def apply(n: Int): Natural = if (n >= 0) Natural.apply(n) else throw new IllegalArgumentException("Negative number not allowed")
+  def apply(n: Long): Natural = if (n >= 0) Natural.apply(n) else throw new IllegalArgumentException("Negative number not allowed")
+
+  // TODO: check if BigInt >= 0 is ok or not?
+  def apply(n: BigInt): Natural = if (n >= 0) Natural.apply(n) else throw new IllegalArgumentException("Negative number not allowed")
+}
+
+given NatFromDigits: FromDigits[Natural] {
+  override def fromDigits(digits: String): Natural = {
+    if (digits.startsWith("-")) throw NumberTooSmall()
+    Natural.apply(digits)
+  }
+}
