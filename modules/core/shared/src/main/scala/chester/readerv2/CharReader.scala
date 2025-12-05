576d465f7afbdf59457abb5a831b52fa5daec39a
Mio <mio@mio19.uk>
2025-12-05 12:17:23 +1100

    s

---
+++ modules/core/shared/src/main/scala/chester/readerv2/CharReader.scala
@@ -0,0 +1,26 @@
+package chester.readerv2
+
+import chester.error.{Span, SpanInFile}
+import chester.reader.{Offset, ParseError, Source}
+import chester.readerv2.StringChar
+import chester.utils.getCodePoints
+ import scala.language.experimental.genericNumberLiterals
+
+import scala.collection.mutable.ArrayBuffer
+
+object CharReader {
+  private def reading(strings: Seq[String], source: Source, offset: Offset): LazyList[StringChar] = {
+    if (strings.isEmpty) return LazyList.empty
+    val head = strings.head
+    val tail = strings.tail
+    val chars = ArrayBuffer.empty[StringChar]
+    var currentOffset = offset
+    for (codePoint <- getCodePoints(head)) {
+      val begin = currentOffset
+      currentOffset = begin.next(codePoint)
+      chars += StringChar(codePoint, Span(source, SpanInFile(currentOffset.getPos, currentOffset.getPos)))
+    }
+    LazyList.from(chars) #::: reading(tail, source, currentOffset)
+  }
+  def read(source: Source): Either[ParseError, Seq[StringChar]] = source.readContent.map(strings => reading(strings, source, source.offset))
+}
