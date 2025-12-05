dd934c6f4cf291223327d672e1c12ab5469b4b7b
Mio <mio@mio19.uk>
2025-12-05 11:46:20 +1100

    Concrete Syntax Tree

--- modules/core/shared/src/main/scala/chester/core/CST.scala
+++ modules/core/shared/src/main/scala/chester/core/CST.scala
@@ -6,16 +6,16 @@ import chester.utils.doc.{*, given}
 import chester.utils.doc.Docs.*
 import upickle.default.*
 
-/** Simple AST using Scala 3 enum */
-enum AST(val span: Span) extends ToDoc derives ReadWriter:
+/** Simple Concrete Syntax Tree using Scala 3 enum */
+enum CST(val span: Span) extends ToDoc derives ReadWriter:
   /** A symbol reference */
-  case Symbol(name: String, override val span: Span) extends AST(span)
+  case Symbol(name: String, override val span: Span) extends CST(span)
   
   /** Function application */
-  case Apply(function: AST, arguments: List[AST], override val span: Span) extends AST(span)
+  case Apply(function: CST, arguments: List[CST], override val span: Span) extends CST(span)
   
   def toDoc(using options: DocConf): Doc = this match
-    case AST.Symbol(name, _) => 
+    case CST.Symbol(name, _) =>
       text(name)
-    case AST.Apply(function, arguments, _) =>
+    case CST.Apply(function, arguments, _) =>
       function.toDoc <> parens(hsep(arguments.map(_.toDoc), `,`))
