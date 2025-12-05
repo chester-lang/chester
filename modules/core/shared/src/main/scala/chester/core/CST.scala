ab79a2ea78e92253df4c08adbc49aef4ddd588ca
Mio <mio@mio19.uk>
2025-12-05 14:20:09 +1100

    ssave

--- modules/core/shared/src/main/scala/chester/core/CST.scala
+++ modules/core/shared/src/main/scala/chester/core/CST.scala
@@ -11,11 +11,27 @@ enum CST(val span: Span) extends ToDoc derives ReadWriter:
   /** A symbol reference */
   case Symbol(name: String, override val span: Span) extends CST(span)
   
-  /** Function application */
-  case Apply(function: CST, arguments: List[CST], override val span: Span) extends CST(span)
+  // (a,b)
+  case Tuple(elements: Vector[CST], override val span: Span) extends CST(span)
+  // [a,b]
+  case ListLiteral(elements: Vector[CST], override val span: Span) extends CST(span)
+
+  case StringLiteral(value: String, override val span: Span) extends CST(span)
+  case IntegerLiteral(value: BigInt, override val span: Span) extends CST(span)
+
+// a b
+  case SeqOf(elements: Vector[CST], override val span: Span) extends CST(span)
   
   def toDoc(using options: DocConf): Doc = this match
     case CST.Symbol(name, _) =>
       text(name)
-    case CST.Apply(function, arguments, _) =>
-      function.toDoc <> parens(hsep(arguments.map(_.toDoc), `,`))
+    case CST.Tuple(elements, _) =>
+      parens(hsep(elements.map(_.toDoc), `,`))
+    case CST.ListLiteral(elements, _) =>
+      brackets(hsep(elements.map(_.toDoc), `,`))
+    case CST.StringLiteral(value, _) =>
+      text("\"") <> text(value) <> text("\"")
+    case CST.IntegerLiteral(value, _) =>
+      text(value.toString)
+    case CST.SeqOf(elements, _) =>
+      hsep(elements.map(_.toDoc), text(" "))
