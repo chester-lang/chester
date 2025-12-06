a4c10307a954314c14f68b24a1ac65c6cd583211
Mio <mio@mio19.uk>
2025-12-06 17:42:40 +1100

    add

---
+++ modules/utils/shared/src/main/scala/chester/uniqid/Uniqid.scala
@@ -0,0 +1,127 @@
+package chester.uniqid
+
+import upickle.default.*
+import spire.math.Natural
+import chester.utils.{Nat, asInt, given}
+import scala.language.experimental.genericNumberLiterals
+
+import java.util.concurrent.atomic.AtomicInteger
+import scala.collection.mutable
+import scala.language.strictEquality
+
+private val uniqIdCounter = new AtomicInteger(0)
+
+type Uniqid = UniqidOf[Any]
+
+private val rwUniqID: ReadWriter[UniqidOf[Any]] = readwriter[Int].bimap(
+  _.id.asInt,
+  x => UniqidOf(Nat(x))
+)
+
+implicit inline def rwUniqIDOf[T]: ReadWriter[UniqidOf[T]] = rwUniqID.asInstanceOf[ReadWriter[UniqidOf[T]]]
+
+case class UniqidOf[+A] private[uniqid] (id: spire.math.Natural) derives CanEqual {}
+
+type UniqidOffset = spire.math.Natural
+
+case class UniqIdRange(start: UniqidOffset, end: UniqidOffset) derives ReadWriter {
+  require(start <= end, s"Invalid range: $start > $end")
+
+  def size: spire.math.Natural = end - start
+}
+
+extension (x: Uniqid) {
+  def asof[T]: UniqidOf[T] = x.asInstanceOf[UniqidOf[T]]
+}
+
+extension [T](x: UniqidOf[T]) {
+  def asid: Uniqid = x
+  def rerange(current: UniqIdRange, target: UniqIdRange): UniqidOf[T] = {
+    require(
+      current.start <= x.id && x.id < current.end,
+      s"Invalid range: $current, $x"
+    )
+    val offset = x.id - current.start
+    UniqidOf(target.start + offset)
+  }
+}
+
+trait UCollector {
+  def apply[T](x: UniqidOf[T]): Unit = ()
+}
+
+trait CollectUniqId extends Any {
+  def collectU(collector: UCollector): Unit
+}
+
+trait UReplacer {
+  def apply[T](x: UniqidOf[T]): UniqidOf[T] = x
+}
+
+trait RerangeUniqid extends Any {
+  def replaceU(reranger: UReplacer): Any
+}
+
+trait ContainsUniqid extends Any with CollectUniqId with RerangeUniqid {
+  lazy val uniqIdRange: UniqIdRange = Uniqid.calculateRange(this)
+}
+
+trait OnlyHasUniqid extends Any {
+  def uniqId: Uniqid
+}
+
+trait HasUniqid extends Any with ContainsUniqid with OnlyHasUniqid {}
+
+object Uniqid {
+  def make[T]: UniqidOf[T] = UniqidOf(Nat(uniqIdCounter.getAndIncrement()))
+
+  def requireRange(size: spire.math.Natural): UniqIdRange = {
+    val sizeInt = size.asInt
+    val start = uniqIdCounter.getAndAdd(sizeInt)
+    UniqIdRange(Nat(start), Nat(start + sizeInt))
+  }
+
+  def currentOffset(): UniqidOffset = Nat(uniqIdCounter.get())
+
+  def captureRange[T](f: => T): (UniqIdRange, T) = {
+    val start = currentOffset()
+    val result = f
+    val end = currentOffset()
+    (UniqIdRange(start, end), result)
+  }
+
+  def is(x: Any): Boolean = x match {
+    case UniqidOf(_) => true
+    case _           => false
+  }
+
+  def calculateRange[T <: ContainsUniqid](x: T): UniqIdRange = {
+    val currentRangeCollect = new mutable.ArrayDeque[Uniqid]()
+    val collecter: UCollector = new UCollector {
+      override def apply[T](id: UniqidOf[T]): Unit =
+        currentRangeCollect.append(id)
+    }
+    x.collectU(collecter)
+    import spire.compat.ordering
+    if (currentRangeCollect.isEmpty) UniqIdRange(currentOffset(), currentOffset())
+    else UniqIdRange(currentRangeCollect.map(_.id).min, currentRangeCollect.map(_.id).max + (1: Natural))
+  }
+
+  case class GiveNewRangeResult[T <: ContainsUniqid](
+      oldRange: UniqIdRange,
+      newRange: UniqIdRange,
+      result: T
+  )
+
+  def giveNewRange[T <: ContainsUniqid](
+      x: T
+  ): GiveNewRangeResult[T] = {
+    val currentRange = x.uniqIdRange
+    val newRange = requireRange(currentRange.size)
+    val reranger: UReplacer = new UReplacer {
+      override def apply[U](id: UniqidOf[U]): UniqidOf[U] =
+        id.rerange(currentRange, newRange)
+    }
+    GiveNewRangeResult(currentRange, newRange, x.replaceU(reranger).asInstanceOf[T])
+  }
+}
--- modules/utils/shared/src/main/scala/chester/utils/asInt.scala
+++ modules/utils/shared/src/main/scala/chester/utils/asInt.scala
@@ -1,16 +1,17 @@
 package chester.utils
 
-import spire.math.{Natural, UInt}
+import spire.math.Natural
+
+def Nat(n: Int): Natural = Natural(n.toLong)
+def Nat(n: Long): Natural = Natural(n)
 
 private val MaxInt = Nat(Integer.MAX_VALUE)
-private val MaxIntUInt = UInt(Integer.MAX_VALUE)
 
 extension (n: Natural) {
   def asInt: Int = if (n <= MaxInt) n.toInt else throw new IllegalArgumentException("Natural is too large to fit in an Int")
 }
 
 extension (n: Double) {
-  // TODO: make more safe
   inline def asInt: Int = n.toInt
 }
 
@@ -18,25 +19,10 @@ extension (n: BigInt) {
   def asInt: Int = if (n.isValidInt) n.toInt else throw new IllegalArgumentException("BigInt is too large to fit in an Int")
 }
 
-extension (n: UInt) {
-  def asInt: Int = if (n <= MaxIntUInt) n.toInt else throw new IllegalArgumentException("UInt is too large to fit in an Int")
-}
-
-// Char is 16bit - always safe
 extension (c: Char) {
   def asInt: Int = c.toInt
 }
 
 extension (s: String) {
   def asInt: Int = s.toInt
-}
-
-// for Scala2
-object AsInt {
-  implicit class AsIntFor(private val n: BigInt) extends AnyVal {
-    def asInt: Int = if (n.isValidInt) n.toInt else throw new IllegalArgumentException("BigInt is too large to fit in an Int")
-  }
-  implicit class AsIntForLong(private val n: Long) extends AnyVal {
-    def asInt: Int = if (n.isValidInt) n.toInt else throw new IllegalArgumentException("Long is too large to fit in an Int")
-  }
 }
