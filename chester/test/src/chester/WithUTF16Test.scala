970591f586286aa73562f9ce776230410f0c14d8
Mio <mio@mio19.uk>
2026-07-01 12:10:41 +1200

    Utilize genericNumberLiterals in test suite

--- chester/test/src/chester/WithUTF16Test.scala
+++ chester/test/src/chester/WithUTF16Test.scala
@@ -1,5 +1,7 @@
 package chester
 
+import scala.language.experimental.genericNumberLiterals
+
 import chester.error.*
 import chester.utils.*
 import chester.utils.given
@@ -18,8 +20,8 @@ class WithUTF16Test extends munit.FunSuite {
     assert(one.nonZero)
 
     val two = one + one
-    assertEquals(two.unicode, Nat(2))
-    assertEquals(two.utf16, Nat(2))
+    assertEquals(two.unicode, 2: Natural)
+    assertEquals(two.utf16, 2: Natural)
 
     assert(zero < one)
     assert(one <= two)
@@ -28,26 +30,24 @@ class WithUTF16Test extends munit.FunSuite {
   }
 
   test("genericNumberLiterals support") {
-    import scala.language.experimental.genericNumberLiterals
-
     // Under genericNumberLiterals and the NatFromDigits given instance,
     // integer literals automatically convert to spire.math.Natural.
     val n: Natural = 12345
-    assertEquals(n, Nat(12345))
+    assertEquals(n, 12345: Natural)
 
     // WithUTF16 expects (spire.math.Natural, spire.math.Natural)
     val w = WithUTF16(10, 20)
-    assertEquals(w.unicode, Nat(10))
-    assertEquals(w.utf16, Nat(20))
+    assertEquals(w.unicode, 10: Natural)
+    assertEquals(w.utf16, 20: Natural)
 
     // Pos expects (WithUTF16, spire.math.Natural, WithUTF16)
     val p = Pos(WithUTF16(0, 0), 100, WithUTF16(5, 5))
-    assertEquals(p.line, Nat(100))
+    assertEquals(p.line, 100: Natural)
   }
 
   test("Pos and SpanInFile serialization") {
-    val pos1 = Pos(WithUTF16(Nat(0), Nat(0)), Nat(0), WithUTF16(Nat(0), Nat(0)))
-    val pos2 = Pos(WithUTF16(Nat(10), Nat(15)), Nat(1), WithUTF16(Nat(5), Nat(7)))
+    val pos1 = Pos(WithUTF16(0, 0), 0, WithUTF16(0, 0))
+    val pos2 = Pos(WithUTF16(10, 15), 1, WithUTF16(5, 7))
     val span = SpanInFile(pos1, pos2)
 
     val serializedPos = write[Pos](pos2)
@@ -68,12 +68,12 @@ class WithUTF16Test extends munit.FunSuite {
       offset = offset.next(char.toInt)
     }
 
-    // Now offset should point to line 1 (0-indexed), pos 6 (unicode/utf16), column 0
-    assertEquals(offset.lineOffset, Nat(1))
-    assertEquals(offset.posOffset, WithUTF16(Nat(6), Nat(6)))
+    // Now offset should point to line 1, pos 6 (unicode/utf16), column 0
+    assertEquals(offset.lineOffset, 1: Natural)
+    assertEquals(offset.posOffset, WithUTF16(6, 6))
     
     val startPos = Pos.zero
-    val endPos = Pos(WithUTF16(Nat(5), Nat(5)), Nat(0), WithUTF16(Nat(5), Nat(5)))
+    val endPos = Pos(WithUTF16(5, 5), 0, WithUTF16(5, 5))
     val span = Span(source, SpanInFile(startPos, endPos))
 
     val lines = span.getLinesInRange
